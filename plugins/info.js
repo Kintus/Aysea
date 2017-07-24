fdad251ea243cf3236de0ce82760d0603db721ba
Kintus <steampunkhoube@gmail.com>
2017-07-24 08:36:02 -0400

    Start

---
+++ plugins/info.js
@@ -0,0 +1,387 @@
+//=====Change Text Content=====
+function changeTextGeneral() {
+    $("#textContent").html(
+        "<div class='accordianBox'>" +
+            "<input class='accordianInput' id='general1' type='checkbox'>" +
+            "<label class='accordianLabel' for='general1'>-Description</label>" +
+            "<div class='accordianContent' overflow='scroll'>" +
+                "<p>" + 
+                    "&nbsp&nbsp&nbsp&nbsp Aysea is an expansive continent, divided between north and south " +
+                    "by a large, dangerous mountain range. The northerners are more prosperous thanks to their large " +
+                    "supplies of metal. The southerners have smaller and less rich settlements, but do well for themselves " +
+                    "thanks to their fertile soil and good farming conditions. The cities in Aysea are rather " +
+                    "extravagant and populous, and they are cultural hubs. They have become hubs due to the massive amounts " +
+                    "of trade between all the kingdoms and their cities. For the last few centuries the kingdoms have " +
+                    "generally been at peace with one another, focusing on expanding into the dense, dangerous mountain  " +
+                    "range or into the sea. However, a few of the royal families have recently gotten into an ongoing " +
+                    "dispute, which has raised tensions, although no action has been taken. The constant " +
+                    "expansion has led to many towns and villages that do not get the resources they need to keep " +
+                    "growing. These settlements can survive, but have stagnated quite significantly, and many " +
+                    "are destroyed thanks to the apparently endless supply of monsters originating from the mountains. " +
+                    "Although the kingdoms have taken over most of the land, there are a few tribes that manage " +
+                    "to survive outside of the kingdoms. These are either tribes of animal people, or they are " +
+                    "groups of outcasts and outlaws. Some of these outcast civilizations are many generations " +
+                    "old, often times with only the descendants of criminals remaining. The tribes of animal people " +
+                    "are generally more like large hamlets, and are largely located in the southern marshes " +
+                    "and in the dangerous mountain ranges. The inhabitants of both tribes are sometimes able " +
+                    "to integrate into cities and towns, but there is a good amount of prejudice against them. " +
+                    "This is especially true for the humanoid animals, so their numbers within the kingdoms " +
+                    "is growing pretty slowly." +
+                "</p>" +
+            "</div>" +
+        "</div>" +
+        "<div class='accordianBox'>" +
+            "<input class='accordianInput' id='general2' type='checkbox'>" +
+            "<label class='accordianLabel' for='general2'>-Races</label>" +
+            "<div class='accordianContent'>" +
+                "<p>" +
+                    "<u>Common:</u><br>" +
+                    "&nbsp&nbsp&nbsp&nbsp Human, Dwarf, Halfling, Goblin, Orc<br>" +
+                    "<u>Uncommon:</u><br>" +
+                    "&nbsp&nbsp&nbsp&nbsp Elf, Kobold, Catfolk, Tengu, Tiefling<br>" +
+                    "<u>Rare:</u><br>" +
+                    "&nbsp&nbsp&nbsp&nbsp Gnome, Half-Orc, Half-Elf, Ratfolk, Kitsune, Grippli, Strix, " +
+                    "Vanara, Nagaji, Merfolk<br>" +
+                "</p>" +
+            "</div>" +
+        "</div>"
+    );
+}
+
+function changeTextReligion() {
+    $("#textContent").html(
+        "<div><h2><u>The Three Creators</u></h2></div>" +
+        "<div class='accordianBox'>" +
+            "<input class='accordianInput' id='religion1' type='checkbox'>" +
+            "<label class='accordianLabel' for='religion1'>-Skepari (Logos, Sol)</label>" +
+            "<div class='accordianContent'>" +
+                "<p>" +
+                    "More information required" +
+                "</p>" +
+            "</div>" +
+        "</div>" +
+        "<div class='accordianBox'>" +
+            "<input class='accordianInput' id='religion2' type='checkbox'>" +
+            "<label class='accordianLabel' for='religion2'>-Lozhu (Ethos, Luna)</label>" +
+            "<div class='accordianContent'>" +
+                "<p>" +
+                    "More information required" +
+                "</p>" +
+            "</div>" +
+        "</div>" +
+        "<div class='accordianBox'>" +
+            "<input class='accordianInput' id='religion3' type='checkbox'>" +
+            "<label class='accordianLabel' for='religion3'>-Vafir (Pathos, Aether)</label>" +
+            "<div class='accordianContent'>" +
+                "<p>" +
+                    "More information required" +
+                "</p>" +
+            "</div>" +
+        "</div>" +
+        "<div><h2><u>The Original Gods</u></h2></div>" +
+        "<div class='accordianBox'>" +
+            "<input class='accordianInput' id='religion4' type='checkbox'>" +
+            "<label class='accordianLabel' for='religion4'>-Dulmir (The King, LG)</label>" +
+            "<div class='accordianContent'>" +
+                "<p>" +
+                    "More information required" +
+                "</p>" +
+            "</div>" +
+        "</div>" +
+        "<div class='accordianBox'>" +
+            "<input class='accordianInput' id='religion5' type='checkbox'>" +
+            "<label class='accordianLabel' for='religion5'>-Kostre (The Warrior, CG)</label>" +
+            "<div class='accordianContent'>" +
+                "<p>" +
+                    "More information required" +
+                "</p>" +
+            "</div>" +
+        "</div>" +
+        "<div class='accordianBox'>" +
+            "<input class='accordianInput' id='religion6' type='checkbox'>" +
+            "<label class='accordianLabel' for='religion6'>-Midla (The Mediator, LN)</label>" +
+            "<div class='accordianContent'>" +
+                "<p>" +
+                    "More information required" +
+                "</p>" +
+            "</div>" +
+        "</div>" +
+        "<div class='accordianBox'>" +
+            "<input class='accordianInput' id='religion7' type='checkbox'>" +
+            "<label class='accordianLabel' for='religion7'>-Faril (The Vagabond, CN)</label>" +
+            "<div class='accordianContent'>" +
+                "<p>" +
+                    "More information required" +
+                "</p>" +
+            "</div>" +
+        "</div>" +
+        "<div class='accordianBox'>" +
+            "<input class='accordianInput' id='religion8' type='checkbox'>" +
+            "<label class='accordianLabel' for='religion8'>-Higgya (The Observer, N)</label>" +
+            "<div class='accordianContent'>" +
+                "<p>" +
+                    "More information required" +
+                "</p>" +
+            "</div>" +
+        "</div>" +
+        "<div class='accordianBox'>" +
+            "<input class='accordianInput' id='religion9' type='checkbox'>" +
+            "<label class='accordianLabel' for='religion9'>-Gaeten (The Healer, NG)</label>" +
+            "<div class='accordianContent'>" +
+                "<p>" +
+                    "More information required" +
+                "</p>" +
+            "</div>" +
+        "</div>" +
+        "<div class='accordianBox'>" +
+            "<input class='accordianInput' id='religion10' type='checkbox'>" +
+            "<label class='accordianLabel' for='religion10'>-Stahlu (The Thief, NE)</label>" +
+            "<div class='accordianContent'>" +
+                "<p>" +
+                    "More information required" +
+                "</p>" +
+            "</div>" +
+        "</div>" +
+        "<div class='accordianBox'>" +
+            "<input class='accordianInput' id='religion11' type='checkbox'>" +
+            "<label class='accordianLabel' for='religion11'>-Ofirik (The Tyrant, LE)</label>" +
+            "<div class='accordianContent'>" +
+                "<p>" +
+                    "More information required" +
+                "</p>" +
+            "</div>" +
+        "</div>" +
+        "<div class='accordianBox'>" +
+            "<input class='accordianInput' id='religion12' type='checkbox'>" +
+            "<label class='accordianLabel' for='religion12'>-Agirni (The Profligate, CE)</label>" +
+            "<div class='accordianContent'>" +
+                "<p>" +
+                    "More information required" +
+                "</p>" +
+            "</div>" +
+        "</div>" +
+        "<div><h2><u>The Contemporary Gods</u></h2></div>" +
+        "<div class='accordianBox'>" +
+            "<input class='accordianInput' id='religion13' type='checkbox'>" +
+            "<label class='accordianLabel' for='religion13'>-Afayel (The Servant)</label>" +
+            "<div class='accordianContent'>" +
+                "<p>" +
+                    "More information required" +
+                "</p>" +
+            "</div>" +
+        "</div>" +
+        "<div class='accordianBox'>" +
+            "<input class='accordianInput' id='religion14' type='checkbox'>" +
+            "<label class='accordianLabel' for='religion14'>-Sivete (The Governor)</label>" +
+            "<div class='accordianContent'>" +
+                "<p>" +
+                    "More information required" +
+                "</p>" +
+            "</div>" +
+        "</div>" +
+        "<div class='accordianBox'>" +
+            "<input class='accordianInput' id='religion15' type='checkbox'>" +
+            "<label class='accordianLabel' for='religion15'>-Holran (The Loyal)</label>" +
+            "<div class='accordianContent'>" +
+                "<p>" +
+                    "More information required" +
+                "</p>" +
+            "</div>" +
+        "</div>" +
+        "<div class='accordianBox'>" +
+            "<input class='accordianInput' id='religion16' type='checkbox'>" +
+            "<label class='accordianLabel' for='religion16'>-Adru (The Merchant)</label>" +
+            "<div class='accordianContent'>" +
+                "<p>" +
+                    "More information required" +
+                "</p>" +
+            "</div>" +
+        "</div>" +
+        "<div class='accordianBox'>" +
+            "<input class='accordianInput' id='religion17' type='checkbox'>" +
+            "<label class='accordianLabel' for='religion17'>-Landeit (The Explorer)</label>" +
+            "<div class='accordianContent'>" +
+                "<p>" +
+                    "More information required" +
+                "</p>" +
+            "</div>" +
+        "</div>" +
+        "<div class='accordianBox'>" +
+            "<input class='accordianInput' id='religion18' type='checkbox'>" +
+            "<label class='accordianLabel' for='religion18'>-Fehrir (The Cordial)</label>" +
+            "<div class='accordianContent'>" +
+                "<p>" +
+                    "More information required" +
+                "</p>" +
+            "</div>" +
+        "</div>" +
+        "<div class='accordianBox'>" +
+            "<input class='accordianInput' id='religion19' type='checkbox'>" +
+            "<label class='accordianLabel' for='religion19'>-Barir (The Victor)</label>" +
+            "<div class='accordianContent'>" +
+                "<p>" +
+                    "More information required" +
+                "</p>" +
+            "</div>" +
+        "</div>" +
+        "<div class='accordianBox'>" +
+            "<input class='accordianInput' id='religion21' type='checkbox'>" +
+            "<label class='accordianLabel' for='religion21'>-Lauknen (The Flora)</label>" +
+            "<div class='accordianContent'>" +
+                "<p>" +
+                    "More information required" +
+                "</p>" +
+            "</div>" +
+        "</div>" +
+        "<div class='accordianBox'>" +
+            "<input class='accordianInput' id='religion20' type='checkbox'>" +
+            "<label class='accordianLabel' for='religion20'>-Luratt (The Fauna)</label>" +
+            "<div class='accordianContent'>" +
+                "<p>" +
+                    "More information required" +
+                "</p>" +
+            "</div>" +
+        "</div>" +
+        "<div class='accordianBox'>" +
+            "<input class='accordianInput' id='religion22' type='checkbox'>" +
+            "<label class='accordianLabel' for='religion22'>-Stiath (The Guide)</label>" +
+            "<div class='accordianContent'>" +
+                "<p>" +
+                    "More information required" +
+                "</p>" +
+            "</div>" +
+        "</div>" +
+        "<div class='accordianBox'>" +
+            "<input class='accordianInput' id='religion23' type='checkbox'>" +
+            "<label class='accordianLabel' for='religion23'>-Ginere (The Abyss)</label>" +
+            "<div class='accordianContent'>" +
+                "<p>" +
+                    "More information required" +
+                "</p>" +
+            "</div>" +
+        "</div>" +
+        "<div class='accordianBox'>" +
+            "<input class='accordianInput' id='religion24' type='checkbox'>" +
+            "<label class='accordianLabel' for='religion24'>-Aldartal (The Patient)</label>" +
+            "<div class='accordianContent'>" +
+                "<p>" +
+                    "More information required" +
+                "</p>" +
+            "</div>" +
+        "</div>" +
+        "<div class='accordianBox'>" +
+            "<input class='accordianInput' id='religion25' type='checkbox'>" +
+            "<label class='accordianLabel' for='religion25'>-Halro (The Shield)</label>" +
+            "<div class='accordianContent'>" +
+                "<p>" +
+                    "More information required" +
+                "</p>" +
+            "</div>" +
+        "</div>" +
+        "<div class='accordianBox'>" +
+            "<input class='accordianInput' id='religion26' type='checkbox'>" +
+            "<label class='accordianLabel' for='religion26'>-Lirthen (The Muse)</label>" +
+            "<div class='accordianContent'>" +
+                "<p>" +
+                    "More information required" +
+                "</p>" +
+            "</div>" +
+        "</div>" +
+        "<div class='accordianBox'>" +
+            "<input class='accordianInput' id='religion27' type='checkbox'>" +
+            "<label class='accordianLabel' for='religion27'>-Anvit (The Scholar)</label>" +
+            "<div class='accordianContent'>" +
+                "<p>" +
+                    "More information required" +
+                "</p>" +
+            "</div>" +
+        "</div>" +
+        "<div class='accordianBox'>" +
+            "<input class='accordianInput' id='religion28' type='checkbox'>" +
+            "<label class='accordianLabel' for='religion28'>-Firdue (The Cycle)</label>" +
+            "<div class='accordianContent'>" +
+                "<p>" +
+                    "More information required" +
+                "</p>" +
+            "</div>" +
+        "</div>" +
+        "<div class='accordianBox'>" +
+            "<input class='accordianInput' id='religion29' type='checkbox'>" +
+            "<label class='accordianLabel' for='religion29'>-Vaythus (The Empty)</label>" +
+            "<div class='accordianContent'>" +
+                "<p>" +
+                    "More information required" +
+                "</p>" +
+            "</div>" +
+        "</div>" 
+    );
+}
+    
+function changeTextKingdoms() {
+    $("#textContent").html(
+        "<div class='accordianBox'>" +
+            "<input class='accordianInput' id='kingdoms1' type='checkbox'>" +
+            "<label class='accordianLabel' for='kingdoms1'>-The Luminor Empire</label>" +
+            "<div class='accordianContent'>" +
+                "<p>" +
+                    "More information required" +
+                "</p>" +
+            "</div>" +
+        "</div>" +
+        "<div class='accordianBox'>" +
+            "<input class='accordianInput' id='kingdoms2' type='checkbox'>" +
+            "<label class='accordianLabel' for='kingdoms2'>-Odegone Kingdom</label>" +
+            "<div class='accordianContent'>" +
+                "<p>" +
+                    "More information required" +
+                "</p>" +
+            "</div>" +
+        "</div>" +
+        "<div class='accordianBox'>" +
+            "<input class='accordianInput' id='kingdoms3' type='checkbox'>" +
+            "<label class='accordianLabel' for='kingdoms3'>-Fawaluce Kingdom</label>" +
+            "<div class='accordianContent'>" +
+                "<p>" +
+                    "More information required" +
+                "</p>" +
+            "</div>" +
+        "</div>" +
+        "<div class='accordianBox'>" +
+            "<input class='accordianInput' id='kingdoms4' type='checkbox'>" +
+            "<label class='accordianLabel' for='kingdoms4'>-The Auxora Dynasty</label>" +
+            "<div class='accordianContent'>" +
+                "<p>" +
+                    "More information required" +
+                "</p>" +
+            "</div>" +
+        "</div>"
+    );
+}
+
+function changeTextHistory() {
+    $("#textContent").html(
+        "<div class='accordianBox'>" +
+            "<input class='accordianInput' id='history1' type='checkbox'>" +
+            "<label class='accordianLabel' for='history1'>-History</label>" +
+            "<div class='accordianContent'>" +
+                "<p>" +
+                    "More information required" +
+                "</p>" +
+            "</div>" +
+        "</div>"
+    );
+}
+
+function changeTextLegends() {
+    $("#textContent").html(
+        "<div class='accordianBox'>" +
+            "<input class='accordianInput' id='legends1' type='checkbox'>" +
+            "<label class='accordianLabel' for='legends1'>-Legends</label>" +
+            "<div class='accordianContent'>" +
+                "<p>" +
+                    "More information required" +
+                "</p>" +
+            "</div>" +
+        "</div>"
+    );
+}
